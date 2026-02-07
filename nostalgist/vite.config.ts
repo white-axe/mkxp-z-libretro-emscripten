229d7aa51d939874150fc24d44436e5f7b757f47
刘皓 <whiteaxe@tuta.io>
2026-02-07 18:34:10 -0500

    Use `require-corp` instead of `credentialless` COEP in the Vite dev server
    `credentialless` isn't as widely supported as `require-corp` and we
    don't need `credentialless` COEP.

--- nostalgist/vite.config.ts
+++ nostalgist/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "credentialless",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
 });
